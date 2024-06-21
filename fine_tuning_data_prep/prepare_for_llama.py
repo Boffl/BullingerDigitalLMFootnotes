79b96a061227d4379762f33aeb30a67956a8cbcd
nikob <nikolaj.bauer@uzh.ch>
2024-06-21 16:59:37 +0200

    formatting tests, for llama pretrain data

--- fine_tuning_data_prep/prepare_for_llama.py
+++ fine_tuning_data_prep/prepare_for_llama.py
@@ -43,19 +43,39 @@ def prepare_zwingliana(folderpath):
 
 if __name__ == "__main__":
 
-    texts = prepare_zwingliana("../../fine_tuning_data/Zwingliana/converted_txt")
+    # texts = prepare_zwingliana("../../fine_tuning_data/Zwingliana/converted_txt")
 
-    with open("../../fine_tuning_data/zwingliana_llama_test.json", "w", encoding="utf-8") as outjson:
-        json.dump(texts, outjson, indent=4)
+    # with open("../../fine_tuning_data/zwingliana_llama_test.json", "w", encoding="utf-8") as outjson:
+        # json.dump(texts, outjson, indent=4)
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("domain", type=str, choices=["EA", "Zwingliana"])
-    
-    # parser.add_argument("--path", default="../../fine_tuning_data", help="Path to the folder with the finetuning data")
+    # todo: make version to open only one file
+    parser = argparse.ArgumentParser()
+    parser.add_argument("domain", type=str, choices=["EA", "Zwingliana"])
+    parser.add_argument("outfile_name", type=str, help="name for the jsonfile that is produced")
+    parser.add_argument("--folder_path", default="../../fine_tuning_data", help="Path to the folder with the finetuning data")
+    parser.add_argument("--one_file", type=str, help="Just processing one file for testing purposes? Put Filepath here", default="")
 
-    # args = parser.parse_args()
-    # domain = args.dopain
-    # path = args.path
+    args = parser.parse_args()
+    domain = args.domain
+    outfile_name = args.outfile_name
+    folder_path = args.folder_path
+    one_file_path = args.one_file
+
+    if domain == "Zwingliana":
+        zwingliana_path = os.path.join(folder_path, "Zwingliana", "converted_txt")
+        outfile_path = os.path.join(folder_path, outfile_name)
+
+        texts = []
+        if one_file_path:
+            with open(one_file_path, "r", encoding="utf-8") as infile:
+                texts.append({"text": infile.read()})
+                # for text in parse_zwingliana_txt_file(infile):
+                  #  texts.append({"text": text})
+        
+        with open(outfile_path, "w", encoding="utf-8") as outjson:
+            json.dump(texts, outjson)
+        
+
 
     
 
