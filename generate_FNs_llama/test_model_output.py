d779350e5cb0b45ce8cd975ca29fb3bb15bf9a3e
nikob <nikolaj.bauer@uzh.ch>
2024-11-05 13:19:33 +0100

    test adapter combos

--- generate_FNs_llama/test_model_output.py
+++ generate_FNs_llama/test_model_output.py
@@ -1,5 +1,6 @@
 from run_llama_over_prompts import *
 import logging
+from itertools import combinations
 
 
 def test_single_adapters():
@@ -16,7 +17,7 @@ def test_single_adapters():
 
     for adapter in adapters:
         adapter_id = make_adapter_id(adapter, size)
-        model = PeftModel.from_pretrained(model, adapter_id, adapter_name=adapters)
+        model = PeftModel.from_pretrained(model, adapter_id, adapter_name=adapter)
         model.set_adapter(adapter)
 
         generated_text = generate_chat(messages, model, tokenizer)
@@ -68,11 +69,43 @@ def test_all_adapters():
     generated_text = generate_chat(messages,model,tokenizer)
     logging.info(f"Output with {model.active_adapter} adapter:\n{generated_text}")
 
+def test_adapter_combos():
+         # example file
+    filepath = "/data/nbauer/data/prompts/instruct_add_window/dev_100/13134_23.jsonl"
+    with jsonlines.open(filepath) as infile:
+        messages = [line for line in infile]
+    size = 70
+    adapters = ["add", "qa", "EA", "bible"]
+    model_id = f"unsloth/Meta-Llama-3.1-{size}B-Instruct-bnb-4bit"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id)
+
+    # load all adapters
+    adapter_id = make_adapter_id(adapters[0, size])
+    model = PeftModel.from_pretrained(model, adapter_id, adapter_name=adapters[0])
+    for adapter in adapters[1:]:
+        adapter_id = make_adapter_id(adapter, size)
+        model.load_adapter(adapter_id, adapter_name=adapter)
+    
+    # make adapter combos
+    adapter_combos = []
+    for r in [2,3]:
+        adapter_combos.extend(combinations(adapters, r))
+    
+    for combo in adapter_combos:
+        new_adapter = "-".join(combo)
+        model.add_weighted_adapter(list(combo), [1.0]*len(combo), adapter_name=new_adapter, combination_type="linear")
+        model.set(new_adapter)
+        generated_text = generate_chat(messages, model, tokenizer)
+        logging.info(f"Output with {model.active_adapter} adapter:\n{generated_text}")
+
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("test_function", choices=["single_adapters", "all_adapters"])
+    parser.add_argument("test_function", choices=["single_adapters", "all_adapters", "adapter_combo"])
     parser.add_argument("--log_file_name", default="")
     args = parser.parse_args()
     if args.log_file_name == "":
@@ -91,4 +124,6 @@ if __name__ == "__main__":
         test_single_adapters()
     if args.test_function == "all_adapters":
         test_all_adapters()
+    if args.test_function == "adapter_combo":
+        test_adapter_combos()
 
